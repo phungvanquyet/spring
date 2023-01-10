06f94c11fc99d407802e8ae153ca43c6bcd91e3c
quyet.phung <quyet.phung@formos.com>
2023-01-10 15:42:06 +0700

    spring bean scope and life cycle - XML configuration

--- springDemoOne/src/com/pvquyet/springdemo/TrackCoach.java
+++ springDemoOne/src/com/pvquyet/springdemo/TrackCoach.java
@@ -1,16 +1,30 @@
 package com.pvquyet.springdemo;
 
 public class TrackCoach implements Coach {
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {
+	}
+
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 50km";
 	}
-
+	
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFotuneService();
 	}
-
+	
+	public void doMyStartupStuff() {
+		System.out.println("Bean created.");
+	}
+	
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("Bean had been destroyed.");
+	}
 }
