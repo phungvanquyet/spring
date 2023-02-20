47aac6389e420a5a60c00b96465dbccf7502add7
quyet.phung <quyet.phung@formos.com>
2023-02-20 16:23:30 +0700

    spring security password encryption

--- spring-security-demo-02-security/src/main/java/com/pvquyet/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-02-security/src/main/java/com/pvquyet/springsecurity/demo/config/DemoAppConfig.java
@@ -1,16 +1,36 @@
 package com.pvquyet.springsecurity.demo.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.pvquyet.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+	
+	// setup variable to hold properties
+	
+	@Autowired
+	private Environment env;
+	
+	// setup logger for diagnostics
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
 	
 	// define a bean for ViewResolver
 	
@@ -23,4 +43,52 @@ public class DemoAppConfig {
 		return viewResolver;
 	}
 
+	// define a bean for our security datasource
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		// create connection pool
+		ComboPooledDataSource securityDataSource 
+									= new ComboPooledDataSource();
+		
+		// set the jdbc driver class
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		// log the connection props
+		logger.info(">> jdbc.url=" + env.getProperty("jdbc.url"));
+		logger.info(">> jdbc.user=" + env.getProperty("jdbc.user"));
+		
+		// set data connection props
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		// set connection poll props
+		securityDataSource.setInitialPoolSize(
+				getIntProperty("connection.pool.initialPoolSize"));
+		
+		securityDataSource.setMinPoolSize(
+				getIntProperty("connection.pool.minPoolSize"));
+		
+		securityDataSource.setMaxPoolSize(
+				getIntProperty("connection.pool.maxPoolSize"));
+		
+		securityDataSource.setMaxIdleTime(
+				getIntProperty("connection.pool.maxIdleTime"));
+		
+		return securityDataSource;
+	}
+	
+	private int getIntProperty(String propName) {
+		String proVal = env.getProperty(propName);
+		
+		// convert to in
+		int intPropVal = Integer.parseInt(proVal);
+		return intPropVal;
+	}
 }
--- spring-security-demo-02-security/src/test/java/com/pvquyet/springsecurity/demo/AppTest.java
+++ spring-security-demo-02-security/src/test/java/com/pvquyet/springsecurity/demo/AppTest.java
@@ -1,4 +1,4 @@
-package com.luv2code.springsecurity.demo;
+package com.pvquyet.springsecurity.demo;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
